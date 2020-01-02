a1492b39ea5feef97a51573d7d355f716c71aff4
daring-array-6961 <ebrahimnery96@gmail.com>
2020-01-02 21:22:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -89,7 +89,8 @@ function placeOrder(cardNumber) {
   // write your code here
 
   if(cardNumber === 0) {
-    return "Sorry, we don't have a credit card on file for you.";
+    //return "Sorry, we don't have a credit card on file for you.";
+    return "Your total cost is " + "$" + total + ", which will be charged to the card " + cardNumber + ".";
   }
 
   let total = 0;
