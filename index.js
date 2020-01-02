b6a1f5429341c50927f8939ff7a329ed9417c8b7
daring-array-6961 <ebrahimnery96@gmail.com>
2020-01-02 21:42:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -88,10 +88,10 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   // write your code here
 
-  if(cardNumber == "") {
-    return "Sorry, we don't have a credit card on file for you.";
+  //if(cardNumber == "") {
+    //return "Sorry, we don't have a credit card on file for you.";
     //return "Your total cost is " + "$" + total + ", which will be charged to the card " + cardNumber + ".";
-  }
+  //}
 
   let total = 0;
   var int = parseInt(cart.length);
