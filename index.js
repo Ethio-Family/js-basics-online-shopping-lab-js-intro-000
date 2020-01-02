7ffd6951ff3f606194b6d81f969e6c4c34ff45d4
daring-array-6961 <ebrahimnery96@gmail.com>
2020-01-02 21:36:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -88,7 +88,7 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   // write your code here
 
-  if(isEmpty().cardNumber) {
+  if(isEmpty().cardNumber == true) {
     return "Sorry, we don't have a credit card on file for you.";
     //return "Your total cost is " + "$" + total + ", which will be charged to the card " + cardNumber + ".";
   }
